2705e543432bb81f029f37e89b4bfc6903b84717
zaprzemo <zaprzemo+github@gmail.com>
2021-02-26 20:45:40 +0100

    Implemented concern for managing image data

--- app/models/portfolio.rb
+++ app/models/portfolio.rb
@@ -1,4 +1,5 @@
 class Portfolio < ApplicationRecord
+	include Placeholder
 	validates :title, :body, :main_image, :thumb_image, presence: true
 	
 
@@ -11,8 +12,8 @@ class Portfolio < ApplicationRecord
 	after_initialize :set_defaults #initialize after def new
 
 	def set_defaults
-		self.main_image ||= "http://www.placehold.it/600x400"
-		self.thumb_image ||= "http://www.placehold.it/350x200"
+		self.main_image ||= Placeholder.image_generator(height: '600',width: '400')
+		self.thumb_image ||= Placeholder.image_generator(height: '350',width: '200')
 	end
 end
 
--- app/models/skill.rb
+++ app/models/skill.rb
@@ -1,3 +1,10 @@
 class Skill < ApplicationRecord
+	include Placeholder
 	validates :title, :percent_utilized, presence: true
+	
+	after_initialize :set_defaults #initialize after def new
+
+	def set_defaults
+		self.badge ||= Placeholder.image_generator(height: '250',width:'250')
+	end
 end
