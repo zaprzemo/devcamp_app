6d4fe184cdd1e7c25a399ba8fe7d47034edaf091
zaprzemo <zaprzemo+github@gmail.com>
2021-02-27 11:43:57 +0100

    Added session tracker for dynamic source links

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,9 @@
 class ApplicationController < ActionController::Base
 	include DeviseWhitelist
+
+	before_action :set_source
+
+	def set_source
+		session[:source] = params[:q] if params[:q] #session is a standad object		
+	end
 end
