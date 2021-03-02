595daee6b5760f70fff50a601c5d88430b89c336
zaprzemo <zaprzemo+github@gmail.com>
2021-03-02 09:44:32 +0100

    Implemented acts_as_list, stimulus, sortablejs for portfolios

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -17,3 +17,5 @@ import "bootstrap"
 Rails.start()
 Turbolinks.start()
 ActiveStorage.start()
+
+import "controllers"
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,12 @@
 Rails.application.routes.draw do
   
   devise_for :users, path: '', path_names: {sign_in: 'login', sign_out: 'logout', sign_up: 'register'}
-  resources :portfolios, except: [:show]
+  resources :portfolios, except: [:show] do
+    member do
+      patch :move
+    end
+  end
+
   get 'angular-items', to: 'portfolios#angular'
   get 'portfolio/:id', to: 'portfolios#show', as: 'portfolio_show'
   get 'about-me', to: 'pages#about'
