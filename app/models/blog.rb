71d5ca5dd887f6c6de69d7caac483e16c78a3130
zaprzemo <zaprzemo+github@gmail.com>
2021-03-06 12:27:39 +0100

    Implemented redis library and created comment generator

--- app/models/blog.rb
+++ app/models/blog.rb
@@ -1,5 +1,6 @@
 class Blog < ApplicationRecord
 	belongs_to :topic
+  has_many :comments, dependent: :destroy
   enum status: {draft: 0, published: 1}
   extend FriendlyId
   friendly_id :title, use: :slugged
