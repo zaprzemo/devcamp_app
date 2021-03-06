449af91827e427d85be774f155620dde1a161c77
zaprzemo <zaprzemo+github@gmail.com>
2021-03-06 16:55:52 +0100

    Added fa-icon,refactored controller for comments refactored to handle comments delete

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,10 +1,25 @@
 class CommentsController < ApplicationController
+  before_action :set_comment, only: %i[ destroy]
 
-def create
-  @comment = current_user.comments.build(comment_params)
-end
+  def create
+    @comment = current_user.comments.build(comment_params)
+  end
 
-def comment_params
-  params.require(:comment).permit(:content)
-end
+
+  def destroy
+    @comment.destroy
+    respond_to do |format|
+      format.html { redirect_to blog_path(@comment.blog_id), notice: "Comment was successfully removed." }
+      format.json { head :no_content }
+    end
+  end
+
+  private
+  def set_comment
+    @comment = Comment.find(params[:id])
+  end
+
+  def comment_params
+    params.require(:comment).permit(:content)
+  end
 end
