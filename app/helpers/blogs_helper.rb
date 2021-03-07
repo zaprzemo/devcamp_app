90df52588c1b61b3b29e0cfc32c5b6556934429c
zaprzemo <zaprzemo+github@gmail.com>
2021-03-07 16:51:34 +0100

    Implemented markdown for blogs posts

--- app/helpers/blogs_helper.rb
+++ app/helpers/blogs_helper.rb
@@ -2,4 +2,20 @@ module BlogsHelper
   def gravatar_helper user
     image_tag "https://www.gravatar.com/avatar/#{Digest::MD5.hexdigest(user.email)}", width: 60
   end
+  class Codeyarify < Redcarpet::Render::HTML
+    def block_code(code, language)
+      CodeRay.scan(code, language).div
+    end
+  end
+  def markdown(text)
+    codeyarify = Codeyarify.new(filter_html: true, hard_wrap: true)
+    options = {
+      fenced_code_blocks: true,
+      no_intra_emphasis: true,
+      autolink: true,
+      lax_html_blocks: true
+    }
+    markdown_to_html = Redcarpet::Markdown.new(codeyarify, options)
+    markdown_to_html.render(text).html_safe
+  end
 end
