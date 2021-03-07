2a816834ca947f00e910941f0c43a8b95b4eee7d
zaprzemo <zaprzemo+github@gmail.com>
2021-03-07 15:20:41 +0100

    Added validation for topics in blogs and a topic form element

--- app/models/blog.rb
+++ app/models/blog.rb
@@ -4,7 +4,7 @@ class Blog < ApplicationRecord
   enum status: {draft: 0, published: 1}
   extend FriendlyId
   friendly_id :title, use: :slugged
-  validates :title, :body, presence: true
+  validates :title, :body, :topic_id, presence: true
 
   def self.recent
     order(created_at: :desc)
