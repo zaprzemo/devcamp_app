253f9879273d645677e67178302642258d665160
zaprzemo <zaprzemo+github@gmail.com>
2021-03-08 17:58:47 +0100

    Updated source tracking feature

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -10,10 +10,10 @@ module ApplicationHelper
     end
 	end
 
-	def source_helper(layout_name)
+	def source_helper(styles)
 		if session[:source]
-			greeting = "Thanks for visiting me from #{session[:source]} and you are on the #{layout_name} layout."
-			content_tag(:p, greeting, class: "source-greeting")
+			greeting = "Thanks for visiting me from #{session[:source]}, please feel free to #{link_to 'contact me', contact_path} if you would like to work together."
+			content_tag(:div, greeting.html_safe, class: styles)
     end
 	end
   def nav_items
@@ -55,7 +55,7 @@ module ApplicationHelper
   def alerts
     alert = (flash[:alert] || flash[:error] || flash[:notice])
     if alert
-      alert_generator alert      
+      alert_generator alert
     end
   end
   def alert_generator msg
